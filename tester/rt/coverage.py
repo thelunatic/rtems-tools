88e975d810e714c7538f66743a63ccd3e99f9624
Cillian O'Donnell <cpodonnell8@gmail.com>
2017-07-13 18:46:38 +0100

    tester/rt/coverage.py: Add copyright license.

--- tester/rt/coverage.py
+++ tester/rt/coverage.py
@@ -1,8 +1,19 @@
-'''
-Created on Jul 7, 2014
+#
+# Copyright (c) 2014, Krzysztof Miesowicz <krzysztof.miesowicz@gmail.com> 
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 
-@author: Krzysztof Miesowicz <krzysztof.miesowicz@gmail.com>
-'''
 from rtemstoolkit import path
 from rtemstoolkit import log
 from rtemstoolkit import execute
